cec085d09491aabc6b8776981746490831595c6d
Uche Ozoemena <ucheozoemena@gmail.com>
2019-08-07 07:26:17 +0100

    Update autocomplete suggested locations

--- src/default-location-searches.js
+++ src/default-location-searches.js
@@ -9,38 +9,33 @@ const { LatLng, LatLngBounds } = sdkTypes;
 // `predictionPlace` (util.types.place) properties.
 export default [
   {
-    id: 'default-helsinki',
+    id: 'default-atlanta',
     predictionPlace: {
-      address: 'Helsinki, Finland',
-      bounds: new LatLngBounds(new LatLng(60.29783, 25.25448), new LatLng(59.92248, 24.78287)),
+      address: 'Atlanta, USA',
+      bounds: new LatLngBounds(
+        new LatLng(33.8876179, -84.289389),
+        new LatLng(33.6478079, -84.5518189)
+      ),
     },
   },
   {
-    id: 'default-turku',
+    id: 'default-sf-bay-area',
     predictionPlace: {
-      address: 'Turku, Finland',
-      bounds: new LatLngBounds(new LatLng(60.53045, 22.38197), new LatLng(60.33361, 22.06644)),
+      address: 'San Francisco Bay Area, USA',
+      bounds: new LatLngBounds(
+        new LatLng(38.8642448, -121.208178),
+        new LatLng(36.8941549, -123.632497)
+      ),
     },
   },
   {
-    id: 'default-tampere',
+    id: 'default-los-angeles',
     predictionPlace: {
-      address: 'Tampere, Finland',
-      bounds: new LatLngBounds(new LatLng(61.83657, 24.11838), new LatLng(61.42728, 23.5422)),
-    },
-  },
-  {
-    id: 'default-oulu',
-    predictionPlace: {
-      address: 'Oulu, Finland',
-      bounds: new LatLngBounds(new LatLng(65.56434, 26.77069), new LatLng(64.8443, 24.11494)),
-    },
-  },
-  {
-    id: 'default-ruka',
-    predictionPlace: {
-      address: 'Ruka, Finland',
-      bounds: new LatLngBounds(new LatLng(66.16997, 29.16773), new LatLng(66.16095, 29.13572)),
+      address: 'Los Angeles, USA',
+      bounds: new LatLngBounds(
+        new LatLng(34.3373061, -118.1552891),
+        new LatLng(33.7036519, -118.6681759)
+      ),
     },
   },
 ];
