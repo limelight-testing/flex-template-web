21b882333e12d59a32ba4df2935a7e44264353ce
Uche Ozoemena <ucheozoemena@gmail.com>
2019-08-12 21:44:00 +0100

    Update About page

--- src/containers/AboutPage/AboutPage.js
+++ src/containers/AboutPage/AboutPage.js
@@ -12,7 +12,10 @@ import {
 } from '../../components';
 
 import css from './AboutPage.css';
-import image from './about-us-1056.jpg';
+
+// TODO: get an image of the same dimension and use it here
+// import image from './about-us-1056.jpg';
+import image from '../../assets/limelightFacebook-1200x630.jpg';
 
 const AboutPage = () => {
   const { siteTwitterHandle, siteFacebookPage, siteTitle } = config;
@@ -35,48 +38,30 @@ const AboutPage = () => {
         </LayoutWrapperTopbar>
 
         <LayoutWrapperMain className={css.staticPageWrapper}>
-          <h1 className={css.pageTitle}>Experience the unique Finnish home sauna.</h1>
+          <h1 className={css.pageTitle}>The Limelight Internet Showcase</h1>
           <img className={css.coverImage} src={image} alt="My first ice cream." />
 
           <div className={css.contentWrapper}>
             <div className={css.contentSide}>
-              <p>Did you know that Finland has 3.2 million saunas - almost one sauna per person!</p>
+              <p>The Limelight is a marketplace that connects talented entertainment service providers with those who are seeking someone with that skillset.</p>
             </div>
 
             <div className={css.contentMain}>
               <h2>
-                Most of the Finnish saunas are located at the homes of individuals - indeed, most
-                people in Finland live in an apartment with sauna in it. In addition, lots of people
-                have lakeside summer cottages, which also typically come with a separate sauna
-                building near the waterfront.
+                There’s no need for word of mouth recommendations. We are creating a rolodex of professionals who are the best at what they do. What do you need done?
               </h2>
 
               <p>
-                To truly experience a Finnish sauna, you need to look beyond the public saunas, and
-                instead visit a real home or cottage sauna. Saunatime makes this possible for
-                everyone. All our saunas are owned by individuals willing to let tourists and other
-                curious visitors to enter their sacred spaces.
+                Being Paid in Experience / Exposure sucks. Let’s be real, experience is awesome, until you have to pay your bills with it. We are giving users a chance to make money, as they gain the valuable experience that will help to take their careers to the next level. Make your talent your full-time-job.
+                Simply sign up at <ExternalLink href="https://www.limelightmusicmarketplace">www.limelightmusicmarketplace</ExternalLink>, and get to work today!!
               </p>
-
-              <h3 className={css.subtitle}>Are you a sauna owner?</h3>
-
               <p>
-                Saunatime offers you a good way to earn some extra cash! If you're not using your
-                sauna every evening, why not rent it to other people while it's free. And even if
-                you are using your sauna every evening (we understand, it's so good), why not invite
-                other people to join you when the sauna is already warm! A shared sauna experience
-                is often a more fulfilling one.
+                We are not only creating a pool of TOP talent, we are helping you connect to the right person for the job. Save time, money, and energy, by finding the right person for the job the first time. We have a rating system that will allow you to filter and search for the ideal local talent to make your next event or project that way that you want. Now, what can the Limelight do for you?
               </p>
-
-              <h3 id="contact" className={css.subtitle}>
-                Create your own marketplace like Saunatime
-              </h3>
+              <h3 className={css.subtitle}>Are you under-appreciated talent?</h3>
               <p>
-                Saunatime is brought to you by the good folks at{' '}
-                <ExternalLink href="http://sharetribe.com">Sharetribe</ExternalLink>. Would you like
-                to create your own marketplace platform a bit like Saunatime? Or perhaps a mobile
-                app? With Sharetribe it's really easy. If you have a marketplace idea in mind, do
-                get in touch!
+                The Limelight helps you to find the people who are looking for you. As an artist manager, I am often seeking entertainment talent(bg singer, producer, promoter, graphic designer, guitarist, bassist, drummer, keyboardist, dancer, host, DJ, production crew, lighting tech, sound engineer, A/V, videographer, photographer etc); we always have trouble finding the best talent. Sometimes there is no do-over.
+                We built this, because sometimes there is no do-over; this is for the people who can help you get it done the right way, the first time. Welcome to The Limelight.
               </p>
               <p>
                 You can also checkout our{' '}
