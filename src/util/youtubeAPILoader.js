64cd9b2fce2682bbe94d30184edbb7af612bcdf1
Uche Ozoemena <ucheozoemena@gmail.com>
2019-08-12 09:23:58 +0100

    Initiate Youtube API with app config

--- src/util/youtubeAPILoader.js
+++ src/util/youtubeAPILoader.js
@@ -1,5 +1,12 @@
 import config from '../config';
 
-// const youtube = gapi.client.init({ apiKey: config.youtubeAPIKey });
+const getYoutubeAPI = () =>
+  /* eslint-disable-next-line no-undef */
+  gapi.client.init({
+    apiKey: config.youtubeAPIKey,
+    discoveryDocs: [
+      'https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest',
+    ],
+  });
 
-// export default youtube;
+export default { configure: getYoutubeAPI };
