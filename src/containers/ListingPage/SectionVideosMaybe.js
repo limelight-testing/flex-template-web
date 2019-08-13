38221109dfc7e613ebb583404cea222195c99f16
Uche Ozoemena <ucheozoemena@gmail.com>
2019-08-13 20:39:58 +0100

    Add title to videos section

--- src/containers/ListingPage/SectionVideosMaybe.js
+++ src/containers/ListingPage/SectionVideosMaybe.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import { FormattedMessage } from 'react-intl';
+
+import css from './ListingPage.css';
 
 const SectionVideosMaybe = ({ videos, fetchVideosError, fetchVideosInProgress }) => (
-  <div>
+  <div className={css.sectionVideos}>
+    <h2 className={css.videoTitle}>
+      <FormattedMessage id="ListingPage.videoTitle" />
+    </h2>
     Num vids: {videos.length}
     <br />
     Fetching in progress: {fetchVideosInProgress}
